a1b08b12ee35509f43165933ac4d8ccbc0bcc009
Maria Manakhova <47086248+sirenescx@users.noreply.github.com>
2021-08-12 13:10:23 +0300

    #169 solution

---
+++ majority_element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        majority = -1
+        
+        for num in nums:
+            if count == 0:
+                majority = num
+            count += 1 if (num == majority) else -1
+        
+        return majority
