32a8b1ca392efb7c5210407230fe0e3bfadcdb3f
Maria Manakhova <47086248+sirenescx@users.noreply.github.com>
2021-11-05 17:08:10 +0300

    #1791 solution

---
+++ find_center_of_star_graph.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        common: int = 0
+        if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
+            common = edges[0][0]
+        elif edges[0][1] == edges[1][0] or edges[0][1] == edges[1][1]:
+            common = edges[0][1]
+        
+        return common
+            
